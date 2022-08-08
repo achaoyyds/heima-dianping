c3be4248f34fec768f365cf78f25873eea27e082
achao <hello@itcast.cn>
2022-08-08 23:00:55 +0800

    master提交

--- service/serviceVod/src/main/java/com/atguigu/vod/config/MyBatis.java
+++ service/serviceVod/src/main/java/com/atguigu/vod/config/MyBatis.java
@@ -7,5 +7,6 @@ public class MyBatis {
         System.out.println("v1.1");
         System.out.println("master提交");
         System.out.println("v1.2提交");
+        System.out.println("push 提交");
     }
 }
