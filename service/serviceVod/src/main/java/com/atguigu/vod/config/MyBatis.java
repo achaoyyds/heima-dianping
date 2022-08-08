257b03d9cff0bd88d2db08538252e08b5b835b44
achao <hello@itcast.cn>
2022-08-08 21:57:49 +0800

    v1.1

--- service/serviceVod/src/main/java/com/atguigu/vod/config/MyBatis.java
+++ service/serviceVod/src/main/java/com/atguigu/vod/config/MyBatis.java
@@ -3,5 +3,7 @@ package com.atguigu.vod.config;
 public class MyBatis {
     public static void main(String[] args) {
         System.out.println("测试git");
+        System.out.println("v1.1");
+        System.out.println("v1.1");
     }
 }
